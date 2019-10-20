9fcbf14f5f86d8ad029b3d5aba4a36ff3b2feb9f
unknown <ivan.senchanka@gmail.com>
2019-10-20 12:58:16 +0300

    Lesson2

--- src/MyArsenal.java
+++ src/MyArsenal.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class MyArsenal {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("LESSON2");
+
+        int[] arrays = new int[10];
+        for (int i = 0; i < arrays.length; i++) {
+            arrays[i] = (int) ((Math.random()) * 100);
+
+            for (i = 0; i < arrays.length; i += 2) {
+            int max = arrays[i];
+            max = arrays[i];
+        }
+        }
+        System.out.println(Arrays.toString(arrays));
     }
 }
+
+
+
